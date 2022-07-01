92e2521841806d64182bc3ccabc58cae61ac5556
Sasha <sasha.larsonneur@yahoo.fr>
2022-07-01 23:02:06 +0200

    Deuxième commit

--- db/players.js
+++ db/players.js
@@ -2,13 +2,27 @@ const players=[
     {
         name : "Michael Jordan",
         position : "sg",
-        rings : 6
+        rings : 6,
+        teams: ["Chicago Bulls", "Washington Wizards"]
         
     },
     {
         name : "LeBron James",
         position : "sf",
-        rings : 4
+        rings : 4,
+        teams: ["Cleveland Cavaliers", "Miami Heat","Los Angeles Lakers"]
+    },
+    {
+        name : "James Harden",
+        position : "sg",
+        rings : 0,
+        teams: ["Houston Rockets", "Brooklyn Nets"]
+    },
+    {
+        name : "Stephen Curry",
+        position : "pg",
+        rings : 4,
+        teams: ["Golden State Warriors"]
     }
 ]
 module.exports=players
--- endpoints/findplayers.js
+++ endpoints/findplayers.js
@@ -1,9 +1,8 @@
 let players=require('C:/Users/Sasha/Documents/API node/API_NBAplayers/db/players')
 
 module.exports=(app)=>{
-    app.get('/players/:rings', (req,res) => {
-        const rings=parseInt(req.params.rings)
-        res.json(players.find(player=> player.rings === rings)) 
+    app.get('/players/', (req,res) => {
+       res.json(players) 
     })
 }
        
